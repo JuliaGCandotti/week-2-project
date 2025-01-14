01f1843e62001afb812609720b70ff790db18896
JuliaGCandotti <jucandotti@gmail.com>
2025-01-14 10:28:15 -0700

    Week2

---
+++ app/week_2/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function Page() {
+  return (
+    <div>
+      <h1>My app week-2 - page 2</h1>
+      <p><Link href="/">Go to Home</Link></p>
+    </div>
+  );
+}
